84b28694f86f06439855b9bfa1155da827d75c37
Isaac Medina <isaac.meedinaa@gmail.com>
2020-03-25 08:47:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,32 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arr)
+  
+  # while loop solution
+  # i = 0
+  # while i < arr.length do
+  #   p "#{i+1}. #{arr[i]}"
+  #   i += 1
+  # end
+  
+  # each_with_index solution
+  arr.each_with_index do |item, index|
+    puts "#{index + 1}. #{item}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  # new_arr = []
+  # i = 0
+  #
+  # while i < arr.length do
+  #   new_element = "#{arr[i].capitalize}!"
+  #   new_arr.push(new_element)
+  #   i += 1
+  # end
+  #
+  # new_arr
+
+  new_arr = arr.map { |index| "#{index.capitalize}!" }
+  new_arr
 end
 
 def long_planeteer_calls# code an argument here
